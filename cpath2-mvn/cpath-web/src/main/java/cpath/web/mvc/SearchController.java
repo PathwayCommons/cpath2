06debf95293c6386013f7dc7211f0eadf9a92e63
Igor Rodchenkov <rodche@gmail.com>
2010-04-05 11:52:02 -0400

    Switched to Spring 3.0 xml namespaces (+ some re-configuration and cleanup);

--- cpath2-mvn/cpath-web/src/main/java/cpath/web/mvc/SearchController.java
+++ cpath2-mvn/cpath-web/src/main/java/cpath/web/mvc/SearchController.java
@@ -38,7 +38,7 @@ import org.biopax.paxtools.proxy.level3.PhysicalEntityProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
