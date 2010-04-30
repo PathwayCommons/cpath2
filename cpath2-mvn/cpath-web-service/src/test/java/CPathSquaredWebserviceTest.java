006d492adac688af8c92122d36c6c61c7802df69
Igor Rodchenkov <rodche@gmail.com>
2010-04-30 19:28:26 -0400

    cPath web service response schema added and pom.xml confugured to auto-generate (jaxb) classes; Several client examples added to the test classes that make use of the schema...

--- cpath2-mvn/cpath-web-service/src/test/java/CPathSquaredWebserviceTest.java
+++ cpath2-mvn/cpath-web-service/src/test/java/CPathSquaredWebserviceTest.java
@@ -1,7 +1,6 @@
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.*;
 
 import org.junit.Before;
@@ -18,6 +17,9 @@ import org.springframework.web.client.RestTemplate;
 public class CPathSquaredWebserviceTest {
 	
 	static RestTemplate template;
+	//static final String CPATH2_SERVICE_URL = "http://www.pathwaycommons.org/webservice";
+	static final String CPATH2_SERVICE_URL = "http://localhost:8080/cpath-web-service"; // Temp.
+	static final String CPATH_SERVICE_URL = "http://www.pathwaycommons.org/pc/webservice.do";
 	
 	static {
 		template = new RestTemplate();
@@ -34,7 +36,7 @@ public class CPathSquaredWebserviceTest {
 	@Test
 	public void testGetTypes() {
 		
-		String result = template.getForObject("http://localhost:8080/cpath-web-service/types", String.class);
+		String result = template.getForObject(CPATH2_SERVICE_URL+"/types", String.class);
 		assertNotNull(result);
 		System.out.println(result);
 	}
@@ -42,7 +44,7 @@ public class CPathSquaredWebserviceTest {
 
 	@Test
 	public void testGetAllElements() {
-		String result = template.getForObject("http://localhost:8080/cpath-web-service/all/elements", String.class);
+		String result = template.getForObject(CPATH2_SERVICE_URL+"/all/elements", String.class);
 		assertNotNull(result);
 		System.out.println(result);
 	}
@@ -51,7 +53,7 @@ public class CPathSquaredWebserviceTest {
 	@Test
 	public void testGetPathways() {
 		String result = null;
-		result = template.getForObject("http://localhost:8080/cpath-web-service/types/{type}/elements", String.class, "pathway"); 
+		result = template.getForObject(CPATH2_SERVICE_URL+"/types/{type}/elements", String.class, "pathway"); 
 		//note: pathway and Pathway both works (both converted to L3 Pathway class)!
 		assertNotNull(result);
 		assertTrue(result.contains("Pathway50"));
@@ -60,7 +62,7 @@ public class CPathSquaredWebserviceTest {
 
 	@Test
 	public void testGetQueryById1() {
-		String result = template.getForObject("http://localhost:8080/cpath-web-service/elements?uri={uri}", 
+		String result = template.getForObject(CPATH2_SERVICE_URL+"/elements?uri={uri}", 
 				String.class, "http://www.biopax.org/examples/myExample#Pathway50");
 		// http%3A%2F%2Fwww.biopax.org%2Fexamples%2FmyExample%23Pathway50 
 		//? potential URL encoding/decoding problem here (may break NCName): - what if 'GO:12345' used instead 'Pathway50'?
@@ -74,7 +76,7 @@ public class CPathSquaredWebserviceTest {
 	public void testPostQueryById() {
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		map.add("uri", "http://www.biopax.org/examples/myExample#Pathway50");
-		String result = template.postForObject("http://localhost:8080/cpath-web-service/elements", 
+		String result = template.postForObject(CPATH2_SERVICE_URL+"/elements", 
 				map, String.class);
 		assertNotNull(result);
 		System.out.println(result);
