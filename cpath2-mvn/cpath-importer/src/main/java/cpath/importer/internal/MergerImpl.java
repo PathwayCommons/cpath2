aab66704b7f55e13d4d215cc1b22a7181a06196a
Benjamin Gross <Benjamin.Gross@gmail.com>
2010-06-21 14:01:08 -0400

    small molecule merge code complete - testing to follow

--- cpath2-mvn/cpath-importer/src/main/java/cpath/importer/internal/MergerImpl.java
+++ cpath2-mvn/cpath-importer/src/main/java/cpath/importer/internal/MergerImpl.java
@@ -48,6 +48,7 @@ import org.biopax.paxtools.model.level3.UtilityClass;
 import org.biopax.paxtools.model.level3.XReferrable;
 import org.biopax.paxtools.util.ClassFilterSet;
 import org.biopax.paxtools.controller.SimpleMerger;
+import org.biopax.miriam.MiriamLink;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -56,10 +57,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 
 import javax.sql.DataSource;
-
 
 /**
  * Class responsible for Merging pathway data.
@@ -106,6 +108,7 @@ public final class MergerImpl implements Merger {
 	 */
 	@Override
 	public void merge() {
+		
 		SimpleMerger simpleMerger = new SimpleMerger(new SimpleEditorMap(BioPAXLevel.L3));
 
 		// create pc model
@@ -124,45 +127,59 @@ public final class MergerImpl implements Merger {
 				boolean elementExistsInPC = (pcModel.getByID(bpe.getRDFId()) != null);
 
 				// merge all protein/SM references & controlled vocabularies
-				if ((bpe instanceof ProteinReference ||
-					 bpe instanceof SmallMoleculeReference ||
-					 bpe instanceof ControlledVocabulary)
-					&& !elementExistsInPC) 
-				{
+				if ((bpe instanceof ProteinReference ||bpe instanceof ControlledVocabulary)
+					&& !elementExistsInPC) {
+
 					// find specific subclass (e.g. CellVocabulary)!
 					Class<? extends UtilityClass> clazz = 
 						(Class<? extends UtilityClass>) bpe.getModelInterface(); 
 					UtilityClass object = cpathWarehouse.getObject(bpe.getRDFId(), clazz);
 					
 					// if not found by id, - search by UnificationXrefs
-					if(object==null) {
+					if (object==null) {
 						XReferrable r = (XReferrable)bpe; // because PR, SMR, and CV are XReferrable
-						Set<UnificationXref> urefs = new ClassFilterSet<UnificationXref>(
-								r.getXref(), UnificationXref.class);
+						Set<UnificationXref> urefs =
+							new ClassFilterSet<UnificationXref>(r.getXref(), UnificationXref.class);
 						object = cpathWarehouse.getObject(urefs, clazz);
 					}
 					
 					// TODO if not found, shall we search by RelationshipXrefs?.. ;)
 					//...
 					
-					if(object != null) {
+					if (object != null) {
 						pcModel.add(object);
-					} else {
-						// TODO log error, add the bpe as is?
-						pcModel.add(bpe);
-						log.warn(bpe.getRDFId() + 
-							" added 'As Is', because nothing's found in Warehouse.");
 					}
-					
+					else {
+						utilityClassNotFoundInWarehouse(pcModel, bpe);
+					}
+				}
+				else if (bpe instanceof SmallMoleculeReference) {
+
+					// this is a pubchem or chebi small molecule reference
+					// get respective inchi small molecule reference from warehouse
+					Set<SmallMoleculeReference> inchiSmallMoleculeReferences = 
+						getSmallMoleculeReference((SmallMoleculeReference)bpe);
+
+					// did we find inchi SMR in warehouse - should only be one
+					if (inchiSmallMoleculeReferences.size() == 1) {
+						SmallMoleculeReference inchiSMR = inchiSmallMoleculeReferences.iterator().next();
+						// if inchi smr is not already in pc, merge it
+						if (pcModel.getByID(inchiSMR.getRDFId()) == null) {
+							mergeSmallMolecules(pcModel, pathwayModel, (SmallMoleculeReference)bpe, inchiSMR);
+						}
+					}
+					else {
+						// we have a problem
+						utilityClassNotFoundInWarehouse(pcModel, bpe);
+					}
 				}
 			}
-			
+
 			// local merge
 			simpleMerger.merge(pcModel, pathwayModel);
 		}
 		
 		// final merge
-		//pcDAO.importModel(pcModel);
 		simpleMerger.merge(pcDAO, pcModel);
 	}
 
@@ -173,6 +190,7 @@ public final class MergerImpl implements Merger {
 	 * @return Model
 	 */
 	private Model getModel(final Metadata metadata) {
+
 		// get the factory bean (not its product, data source bean)
 		DataServices dataServices = (DataServices) applicationContext.getBean("&cpath2_meta");
 		// create data source
@@ -197,5 +215,106 @@ public final class MergerImpl implements Merger {
 
 		// outta here
 		return toReturn;
+	}
+
+	/**
+	 * Given a SmallMoleculeReference with uxrefs, Find respective 
+	 * SmallMoleculeReferences in warehouse.  Typically, the set returned
+	 * should only contain one smr.  In some cases, two can be returned,
+	 * for example, given an inchi smr, find the pubchem and chebi smrs
+	 * in the warehouse.
+	 *
+	 * @param smrWithUXrefs SmallMoleculeReference
+	 * @return Set<SmallMoleculeReference>
+	 */
+	private Set<SmallMoleculeReference> getSmallMoleculeReference(SmallMoleculeReference smrWithUXrefs) {
+
+		Set<SmallMoleculeReference> toReturn = new HashSet<SmallMoleculeReference>();
+
+		// the set of unification xrefs for this incoming smr
+		Set<UnificationXref> uxrefs = 
+			new ClassFilterSet<UnificationXref>(smrWithUXrefs.getXref(), UnificationXref.class);
+
+		// iterate over all smr uxrefs and look for chebio or pubchem
+		for (UnificationXref uxref : uxrefs) {
+			try {
+				String urn = MiriamLink.getDataTypeURI(uxref.getDb());
+				if (("urn.miriam.chebi").equals(urn) || ("urn.miriam.pubchem").equals(urn)) {
+					// this uxref is to chebi or pubchem.  we search warehouse on each uxref
+					// because the warehouse may find more than one smr if a set of uxrefs
+					// are passed simulataneously.  for example, if we were to pass the set
+					// of uxrefs contains in the inchi smr, we could get back both 
+					// a pubchem and a chebi smr
+					Set<UnificationXref> uxrefsToSearch = new HashSet<UnificationXref>();
+					uxrefsToSearch.add(uxref);
+					SmallMoleculeReference smr =
+						cpathWarehouse.getObject(uxrefsToSearch, SmallMoleculeReference.class);
+					if (smr != null) {
+						toReturn.add(smr);
+					}
+				}
+			}
+			catch (IllegalArgumentException e) {
+				log.error("Unknown or misspelled database name! Won't fix this now... " + e);
+			}
+		}
+
+		// outta here
+		return toReturn;
+	}
+
+	/**
+	 * Given an inchi small molecule reference, find pubchem and chebi small molecule
+	 * references and merge into pc.
+	 * 
+	 *
+	 * @param pcModel Model
+	 * @param pathwayModel Model
+	 * @param incomingSMR SmallMoleculeReference
+	 * @param inchiSMR SmallMoleculeReference
+	 */
+	private void mergeSmallMolecules(Model pcModel, Model pathwayModel,
+									 SmallMoleculeReference incomingSMR, SmallMoleculeReference inchiSMR) {
+
+		// using inchi smr, get pubchem and chebi smr's
+		Set<SmallMoleculeReference> smrs = getSmallMoleculeReference(inchiSMR);
+
+		// sanity check
+		if (smrs.size() == 0) {
+			// we have a problem
+			utilityClassNotFoundInWarehouse(pcModel, inchiSMR);
+			return;
+		}
+		
+		// merge pubchem and chebi info into inchi smr
+		SmallMoleculeMerger smallMoleculeMerger =
+			new SmallMoleculeMerger(new SimpleEditorMap(BioPAXLevel.L3));
+		smallMoleculeMerger.merge(inchiSMR, smrs);
+
+		// add inchi smr into pc
+		pcModel.add(inchiSMR);
+
+		// the follow steps are required for simple merger to work properly
+		// 1) change rdf id of incoming smr to inchi smr id
+		String incomingRDFId = incomingSMR.getRDFId();
+		String inchiRDFId = inchiSMR.getRDFId();
+		incomingSMR.setRDFId(inchiRDFId);
+		// 2) update id map of incoming model and replace id of incoming smr with inchi smr id
+		Map<String, BioPAXElement> pathwayModelMap = pathwayModel.getIdMap();
+		pathwayModelMap.remove(incomingRDFId);
+		pathwayModelMap.put(inchiRDFId, inchiSMR);
+	}
+
+	/**
+	 * Handler called when desired UtilityClass is not found in warehouse.
+	 *
+	 * @param pcModel Model
+	 * @param bpe BioPAXElement
+	 */
+	private void utilityClassNotFoundInWarehouse(Model pcModel, BioPAXElement bpe) {
+
+		// TODO log error, add the bpe as is?
+		pcModel.add(bpe);
+		log.warn(bpe.getRDFId() + " added 'As Is', because nothing's found in Warehouse.");
 	}
 }
