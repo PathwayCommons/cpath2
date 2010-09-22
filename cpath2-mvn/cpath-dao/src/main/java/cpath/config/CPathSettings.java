f2ebb8b3e80f41b2ed66b1c69c6ab875fef1de39
Igor Rodchenkov <rodche@gmail.com>
2010-09-22 19:19:28 -0400

    Half-implemented -fetch-mappingdata feature; updated example metadata configuration; added db name prefix constant to CPathSettings; etc.

--- cpath2-mvn/cpath-dao/src/main/java/cpath/config/CPathSettings.java
+++ cpath2-mvn/cpath-dao/src/main/java/cpath/config/CPathSettings.java
@@ -2,13 +2,17 @@ package cpath.config;
 
 import java.io.File;
 
+
+/*
+ * 
+ */
 public final class CPathSettings {
 	protected CPathSettings(){};
 	
 	public static final String HOME_VARIABLE_NAME = "CPATH2_HOME";
 	
 	/*
-	 * These define "keys" of the DataServicesFactoryBean's 
+	 * These define only the "KEYS" for the DataServicesFactoryBean's 
 	 * data sources map. Values (actual db names) are defined
 	 * in cpath.properties file (in the CPATH2_HOME directory).
 	 * So, these keys are used in the corresponding Spring 
@@ -23,12 +27,22 @@ public final class CPathSettings {
 	public static final String PROTEINS_DB = "cpath2_proteins";
 	public static final String PREMERGE_DB = "premergeDataSource";
 	
-	// PREMERGE_INDEX_DIR_VARIABLE value must match the one used in the 
-	// cpath-importer, internalContext-premerge.xml!
+	/* 
+	 * PREMERGE_INDEX_DIR_VARIABLE value must match the one used in the 
+	 * cpath-importer, internalContext-premerge.xml!
+	 */
 	public static final String PREMERGE_INDEX_DIR_VARIABLE = "premerge.index.dir";
 	
+	/*
+	 * A sub-directory name (within cpath2 home dir) to use
+	 * for fetched mapping data files (e.g., BridgeDb Derby files).
+	 * 
+	 */
 	public static final String MAPPING_DATA_DIR = "idmapping";
 	
+	/*
+	 * Lucene index name for the indexed warehouse entities
+	 */
 	public static final String WHOUSE_SEARCH_INDEX = "cpathwhouse";
 	
 	/*
@@ -38,9 +52,16 @@ public final class CPathSettings {
 	 */
 	public static final String CPATH_URI_PREFIX = "urn:pathwaycommons:";
 	
+	/*
+	 * Use this default prefix for DB names that we create (drop),
+	 * e.g., for pre-merge and unit test databases.
+	 * (this does not affect db names specified in cpath.properties)
+	 */
+	public static final String CPATH_DB_PREFIX = "cpath2_";
 	
 	/**
-	 * Gets the path to the directory with BridgeDb id-mapping files.
+	 * Gets the path to the directory with BridgeDb id-mapping files
+	 * (required by 'idmapper-pgdb' BridgeDb driver)
 	 * 
 	 * @return
 	 */
