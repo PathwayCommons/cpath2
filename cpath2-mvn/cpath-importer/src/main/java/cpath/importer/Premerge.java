e598b443deeddb279a974e4ed0a088242713316f
Benjamin Gross <benjamin.gross@gmail.com>
2010-12-13 12:45:06 -0500

    Removed threading model from premerge process - log4j has difficulties logging from multiple threads and process did not benefit from multithreaded model - ssimplification of code as added benefit

--- cpath2-mvn/cpath-importer/src/main/java/cpath/importer/Premerge.java
+++ cpath2-mvn/cpath-importer/src/main/java/cpath/importer/Premerge.java
@@ -1,25 +1,9 @@
 package cpath.importer;
 
 // imports
-import cpath.importer.internal.PremergeDispatcher;
 import cpath.warehouse.beans.Metadata;
 
-
 public interface Premerge {
-
-	/**
-	 * Called by dispatcher to set pointer back to himself
-	 *
-	 * @param premergeDispatcher PremergeDispatcher
-	 */
-	void setDispatcher(final PremergeDispatcher premergeDispatcher);
-
-	/**
-	 * Called by dispatcher to set metadata object ref. The object to process.
-	 *
-	 * @param metadata Metadata
-	 */
-	void setMetadata(final Metadata metadata);
 
 	/**
 	 * Called to start the premerge process.
