47415c654a55ecc3fa1bda8502b7c8e4914e7ab9
Benjamin Gross <benjamin.gross@gmail.com>
2010-12-23 14:31:02 -0500

    Update to cleaner to replace rdf id with simple algorithm that appends _X_YYYYY, where X is integer chosen to not conflict with existing rdf id's and YYYYY is tax id of the model (derived from biosource of pathway).  Also update to -Xss argument which had to be increased to avoid segfault when importing mouse & rat owls.

--- cpath2-mvn/cpath-importer/src/main/java/cpath/cleaner/internal/BaseCleanerImpl.java
+++ cpath2-mvn/cpath-importer/src/main/java/cpath/cleaner/internal/BaseCleanerImpl.java
@@ -3,6 +3,9 @@ package cpath.cleaner.internal;
 // imports
 import cpath.cleaner.Cleaner;
 
+import org.biopax.paxtools.model.*;
+import org.biopax.paxtools.model.level3.*;
+
 /**
  * Implementation of Cleaner interface for use when data
  * does not need to be cleaned.
@@ -15,5 +18,64 @@ public class BaseCleanerImpl implements Cleaner {
 	 */
 	public String clean(final String pathwayData) {
 		return pathwayData;
+	}
+	
+	/*
+	 * Given a paxtools model for an entire owl file
+	 * returns the tax id for the organism.  This method 
+	 * assumes that at least one pathway in model is annotated
+	 * with a biosource (although all should be).  It also assumes
+	 * that all pathways share the the same biosource.  With that in 
+	 * mind, it returns the first tax id encountered while iterating 
+	 * over each pathways biosource, else returns null.
+	 * 
+	 * @param model Model
+	 * @return String
+	 */
+	protected String getTaxID(final Model model) {
+
+		for (Pathway pathway : model.getObjects(Pathway.class)) {
+			BioSource bioSource = pathway.getOrganism();
+			if (bioSource != null) {
+				for (Xref xref : bioSource.getXref()) {
+					if (xref instanceof UnificationXref) {
+						if (xref.getDb().contains("taxonomy")) {
+							return xref.getId();
+						}
+					}
+				}
+			}
+		}
+		
+		// outta here
+		return null;
+	}
+	
+	/*
+	 * Given a BioPAXElement, returns an RDF Id by
+	 * apply the suffix '_X', where X is an integer to
+	 * the current id.  X is derived by setting it to 1
+	 * and incrementing it (if necessary) until it does
+	 * not clash with an existing RDF Id.  "_" + taxID will
+	 * be appended to rdf id to prevent clashes between across 
+	 * species files supplied by same data provider.
+	 * 
+	 * @param model Model
+	 * @param bpe BioPAXElement
+	 * @param taxID String
+	 * @return String
+	 */
+	protected String getRDFIdReplacement(final Model model, final BioPAXElement bpe, final String taxID) {
+		
+		int inc = 0;
+		String toReturn = "";
+		
+		String currentID = bpe.getRDFId().toUpperCase();
+		do {
+			toReturn = currentID + "_" + ++inc + "_" + taxID;
+		} while (model.containsID(toReturn));
+		
+		// outta here
+		return toReturn;
 	}
 }
