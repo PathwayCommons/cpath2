2de9c1c8ff45e3dc2d7f76635cf7de3e75de73e8
Benjamin Gross <benjamin.gross@gmail.com>
2010-12-22 19:58:26 -0500

    Update to use Model.updateID().  This code will not work ddue to certain reactome elements that do not have xrefs, however I wanted to have record of code changes.  A new version of cleaner is in the works which will changes rdf id by simply append _X to existing id, where X is integer derived by incrementing a counter starting at 1 until the id does not clash with an existing one in the model.  Note, the taxid of the pathway/model will also be appended to the rdf id to prevent id clashes across species files belonging to the same data provider

--- cpath2-mvn/cpath-importer/src/main/java/cpath/cleaner/internal/ReactomeCleanerImpl.java
+++ cpath2-mvn/cpath-importer/src/main/java/cpath/cleaner/internal/ReactomeCleanerImpl.java
@@ -11,6 +11,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.util.Set;
+import java.util.HashSet;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,18 +30,24 @@ import java.io.IOException;
  * <bp:Complex rdf:ID="xxxx">
  * ...
  * ...
+ * <bp:xref rdf:resource="#Reactome167460" />
  * <bp:xref rdf:resource="#REACT_6602.2" />
  * </bp:Complex>
  *
- * replaces rdf:ID="xxxx" with rdf:ID="#REACT_6602.2" -
- * if xref to REACT exists.  All references to element are
- * properly updated with the help of paxtools.
+ * replaces rdf:ID="xxxx" with rdf:ID="#Reactome167460 (if xref exists)
+ * or rdf:ID="#REACT_6602.2" (if xref exists).  All references to element
+ * are properly updated with the help of paxtools.
  *
  * This was motivated by wordy rdf id's used by reactome elements which 
  * may only differ by the case of a single character:
  *
- * http://www.reactome.org/biopax#HIV_1_aborted_elongation_complex_after_arrest__nucleoplasm_
- * http://www.reactome.org/biopax#HIV_1_Aborted_elongation_complex_after_arrest__nucleoplasm_
+ * HIV_1_aborted_elongation_complex_after_arrest__nucleoplasm_
+ * HIV_1_Aborted_elongation_complex_after_arrest__nucleoplasm_
+ *
+ * or
+ *
+ * Active_Calmodulin__name_copied_from_entity_in_Homo_sapiens___nucleoplasm_
+ * active_Calmodulin__name_copied_from_entity_in_Homo_sapiens___nucleoplasm_
  *
  * then subsequently caused primary key conflicts since (by default) mysql does not
  * differenciate between upper and lower case on primary keys.
@@ -50,8 +57,9 @@ public class ReactomeCleanerImpl extends BaseCleanerImpl implements Cleaner {
 	// logger
     private static Log log = LogFactory.getLog(ReactomeCleanerImpl.class);
     
-    private static final String RDF_ID_REPLACEMENT = "urn:miriam:reactome_";
-    private static final Pattern REACTOME_XREF_REGEX = Pattern.compile("REACT_\\d+(\\.\\d+)?");
+    private static final String RDF_ID_REPLACEMENT = "";
+    private static final Pattern REACTOME_XREF_RDFID_REGEX =
+        Pattern.compile("http://www.reactome.org/biopax#(Reactome\\d+)|(REACT_\\d+(\\.\\d+)?)");
 
 	/**
 	 * (non-Javadoc>
@@ -64,29 +72,23 @@ public class ReactomeCleanerImpl extends BaseCleanerImpl implements Cleaner {
 			new BufferedInputStream(new ByteArrayInputStream(pathwayData.getBytes()));
 		SimpleReader simpleReader = new SimpleReader(BioPAXLevel.L3);
 		Model bpModel = simpleReader.convertFromOWL(inputStream);
-		
-		Set<BioPAXElement> sourceElements = bpModel.getObjects();
+
+		Set<Entity> sourceElements = new HashSet<Entity>(bpModel.getObjects(Entity.class));
 		for (BioPAXElement bpe : sourceElements) {
 			if (bpe instanceof XReferrable) {
 				Set<Xref> xrefs = ((XReferrable)bpe).getXref();
 				// look for REACT xref
 				for (Xref xref : xrefs) {
-					if (xref.getId() == null) {
+					if (xref.getRDFId() == null || xref.getId() == null) {
 						if (log.isDebugEnabled()) {
-							log.debug("clean(), Encountered xref without any id!, bpe: " +
+							log.debug("clean(), Encountered xref without any RDFId or id!, bpe: " +
                                       bpe.getRDFId() + " skipping rdf id replacement...");
 						}
 						continue;
 					}
-					Matcher reactXref = REACTOME_XREF_REGEX.matcher(xref.getId());
-	                if (reactXref.find()) {
-	                	String newRDF = RDF_ID_REPLACEMENT + xref.getId();
-	                	String idVersion = xref.getIdVersion();
-	                	if (idVersion != null & idVersion.length() > 0) {
-	                		newRDF += '.' + idVersion;
-	                	}
-	                	// replace rdf of bpe with rdf:ID of xref (should be of the form REACT_XXXX)
-	                	bpe.setRDFId(newRDF);
+                    Matcher reactRDFId = REACTOME_XREF_RDFID_REGEX.matcher(xref.getRDFId());
+	                if (reactRDFId.find()) {
+	                	bpModel.updateID(bpe.getRDFId(), getRDFIdReplacement(xref));
 	                }
 				}
 			}
@@ -108,4 +110,22 @@ public class ReactomeCleanerImpl extends BaseCleanerImpl implements Cleaner {
 		// outta here
 		return outputStream.toString();
 	}
+
+    /**
+     * Given an xref, returns a suitable rdf id replacement.
+     *
+     * @param xref Xref
+     * @return String
+     */
+    private String getRDFIdReplacement(Xref xref) {
+
+        String toReturn = RDF_ID_REPLACEMENT + xref.getId();
+        String idVersion = xref.getIdVersion();
+        if (idVersion != null && idVersion.length() > 0) {
+            toReturn += '.' + idVersion;
+        }
+
+        // outta here
+        return toReturn;
+    }
 }
