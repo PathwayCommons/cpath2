d43035d668be88d5726ca7f4e7c43064a024e560
B. Arman Aksoy <arman@cbio.mskcc.org>
2011-12-16 15:07:00 -0500

    Change entities to entity references for the new SIF export support and add CL synonyms to the list

--- cpath2-mvn/cpath-service/src/main/java/cpath/service/internal/CPathServiceImpl.java
+++ cpath2-mvn/cpath-service/src/main/java/cpath/service/internal/CPathServiceImpl.java
@@ -316,8 +316,8 @@ public class CPathServiceImpl implements CPathService {
 		if (extended) {
 			OutputStream nodeStream = new ByteArrayOutputStream();
 			sic.writeInteractionsInSIFNX(m, edgeStream, nodeStream,
-										 Arrays.asList("Entity/displayName", "Entity/xref:UnificationXref", "Entity/xref:RelationshipXref"),
-										 Arrays.asList("Interaction/dataSource/displayName", "Interaction/xref:PublicationXref"), true);
+			Arrays.asList("EntityReference/displayName", "EntityReference/xref:UnificationXref", "EntityReference/xref:RelationshipXref"),
+			Arrays.asList("Interaction/dataSource/displayName", "Interaction/xref:PublicationXref"), true);
 			String edgeColumns = "PARTICIPANT_A\tINTERACTION_TYPE\tPARTICIPANT_B\tINTERACTION_DATA_SOURCE\tINTERACTION_PUBMED_ID\n";
 			String nodeColumns = "PARTICIPANT\tPARTICIPANT_TYPE\tPARTICIPANT_NAME\tUNIFICATION_XREF\tRELATIONSHIP_XREF\n";
 			map.put(DATA, edgeColumns + removeDuplicateBinaryInteractions(edgeStream) + "\n" + nodeColumns + nodeStream.toString());
