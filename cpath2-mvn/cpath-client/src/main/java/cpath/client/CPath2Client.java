60538a1281d75f412b3b9ad12398cf0e90946ff0
Igor Rodchenkov <rodche@gmail.com>
2012-06-18 17:21:49 -0400

    Added a new client method to get data from valid cpath2 ws 'get' or 'graph' query url (can be built using other public methods). The other thing - it now sets the xml:base (e.g., pathwaycommons.org/pc2/, configured in the cpath2 server instance properties) to BioPAX models returned as a query result.

--- cpath2-mvn/cpath-client/src/main/java/cpath/client/CPath2Client.java
+++ cpath2-mvn/cpath-client/src/main/java/cpath/client/CPath2Client.java
@@ -7,15 +7,17 @@ import cpath.client.util.ServiceResponseHttpMessageConverter;
 import cpath.service.Cmd;
 import cpath.service.CmdArgs;
 import cpath.service.GraphType;
+import cpath.service.OutputFormat;
 import cpath.service.jaxb.*;
 
 import org.apache.commons.lang.StringUtils;
 import org.biopax.paxtools.io.BioPAXIOHandler;
 import org.biopax.paxtools.io.SimpleIOHandler;
 import org.biopax.paxtools.model.Model;
-import org.biopax.paxtools.query.algorithm.Direction;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.*;
 
 /**
@@ -34,8 +36,23 @@ import java.util.*;
 public final class CPath2Client
 {
 	public static final String JVM_PROPERTY_ENDPOINT_URL = "cPath2Url";
-	public static final String DEFAULT_ENDPOINT_URL = "http://awabi.cbio.mskcc.org/pc2/"; //finally - will be at http://www.pathwaycommons.org/pc2/
+	public static final String DEFAULT_ENDPOINT_URL = "http://pathwaycommons.org/pc2/";
 
+	
+	/**
+	 * This is an <em>equivalent</em> to {@link Direction}
+	 * enumeration, and is defined here for convenience (i.e.,
+	 * to free all users of this web service client from 
+	 * depending on paxtools-query module at runtime.)
+	 * 
+	 */
+	//TODO move it to the cpath-api (cause, e.g., OutputFormat is already there)?
+	public static enum Direction
+    {
+		UPSTREAM, DOWNSTREAM, BOTHSTREAM;
+    }
+	
+	
 	private final RestTemplate restTemplate;
 	
 	private String endPointURL;
@@ -45,6 +62,7 @@ public final class CPath2Client
     private Collection<String> dataSources = new HashSet<String>();
     private String type = null;
     private String path = null;
+    private Direction direction = Direction.DOWNSTREAM;
     
     // suppress using constructors in favor of static factories
     private CPath2Client() {
@@ -69,8 +87,11 @@ public final class CPath2Client
     public static CPath2Client newInstance(BioPAXIOHandler bioPAXIOHandler) {
     	CPath2Client client = new CPath2Client(); 
     	
-     	// remove default (xml) message converters (xml root element based, do not work with cpath2 schema)!
-    	client.restTemplate.getMessageConverters().clear();
+    	// DO NOT remove default message converters (not anymore!); because, 
+    	// if we do, 'executeQuery' method below fails if using this 'restTemplate'
+    	// (it will work though with a fresh RestTemplate, with default converters)
+    	//client.restTemplate.getMessageConverters().clear();
+    	
      	// add custom cPath2 XML message converters
     	client.restTemplate.getMessageConverters().add(new ServiceResponseHttpMessageConverter());
     	// add BioPAX http message converter
@@ -84,45 +105,35 @@ public final class CPath2Client
     }
 
     
-    /**
-     * Builds a full-text <em>search</em> query
-     * using current filter values and provided 
-     * keywords (or Lucene query strings)
-     * 
-     * @param keywords
-     * @return
-     */
-    public String querySearch(Collection<String> keywords) {
-    	String url = endPointURL + Cmd.SEARCH + "?" 
-            	+ CmdArgs.q + "=" + join("", keywords, " ") // spaces means 'OR'
-                + (getPage() > 0 ? "&" + CmdArgs.page + "=" + getPage() : "")
-                + (getDataSources().isEmpty() ? "" : "&" + join(CmdArgs.datasource + "=", getDataSources(), "&"))
-                + (getOrganisms().isEmpty() ? "" : "&" + join(CmdArgs.organism + "=", getOrganisms(), "&"))
-                + (getType() != null ? "&" + CmdArgs.type + "=" + getType() : "");
-    	
-    	return url;
-    }  
-
+	/**
+	 * Executes a 'get' and 'graph' type cPath2 
+	 * Web Service API URL query and returns 
+	 * the resulting sub-model (sub-network) as String.
+	 * 
+	 * @see #queryGet(Collection)
+	 * @see #queryNeighborhood(Collection)
+	 * @see #queryCommonStream(Collection)
+	 * @see #queryPathsFromTo(Collection, Collection)
+	 * @see #queryPathsBetween(Collection)
+	 * 
+	 * 
+	 * @param url
+	 * @param outputFormat default is {@link OutputFormat#BIOPAX}
+	 * @return
+	 */
+	public String executeQuery(final String url, final OutputFormat outputFormat) {
+		final String q = (outputFormat == null)
+			? url : url + "&" + CmdArgs.format + "=" + outputFormat;
+		
+		return restTemplate.getForObject(q, String.class);
+	}
     
-    private ServiceResponse searchTemplate(Collection<String> keywords) 
-    		throws CPathException
-    {
-        String url = querySearch(keywords);
-        ServiceResponse resp = restTemplate.getForObject(url, SearchResponse.class);
-        
-        if(resp instanceof ErrorResponse) {
-            throw CPathExceptions.newException((ErrorResponse) resp);
-        }
-        
-        return resp;
-    }
-
     
     /**
-     * Full text search. 
-     * For example, retrieve a list of all records that contain "BRCA2".
+     * Full text search.
+     * @see #search(Collection)
      *
-     * @param keyword keywords (e.g., names or identifiers); can also be a Lucene query
+     * @param keyword keywords (e.g., ); can also be a Lucene query
      * @return 
      * @throws CPathException when the WEB API gives an error
      */
@@ -133,13 +144,38 @@ public final class CPath2Client
     
     /**
      * Full text search. 
+     * 
+     * Retrieve the ordered by score list (hits)
+     * of BioPAX objects that match the search query
+     * and pass currently set filters.
+     * 
+     * See the cPath2 web service online description 
+     * for the list of available index fields and filter values
+     * (currently loaded data sources and organisms).
      *
-     * @param keywords set of keywords (will be joint with 'OR'); each can be a Lucene query
+     * @see #setDataSources(Collection)
+     * @see #setOrganisms(Collection)
+     *
+     * @param keywords names, identifiers, or Lucene queries (will be joint with 'OR')
      * @return
      * @throws CPathException when the WEB API gives an error
      */
-    public ServiceResponse search(Collection<String> keywords) throws CPathException {
-        return searchTemplate(keywords);
+    public ServiceResponse search(Collection<String> keywords) 
+    		throws CPathException 
+    {
+    	String url = endPointURL + Cmd.SEARCH + "?" 
+            	+ CmdArgs.q + "=" + join("", keywords, " ") // spaces means 'OR'
+                + (getPage() > 0 ? "&" + CmdArgs.page + "=" + getPage() : "")
+                + (getDataSources().isEmpty() ? "" : "&" + join(CmdArgs.datasource + "=", getDataSources(), "&"))
+                + (getOrganisms().isEmpty() ? "" : "&" + join(CmdArgs.organism + "=", getOrganisms(), "&"))
+                + (getType() != null ? "&" + CmdArgs.type + "=" + getType() : "");
+    	
+        ServiceResponse resp = restTemplate.getForObject(url, SearchResponse.class);
+        if(resp instanceof ErrorResponse) {
+            throw CPathExceptions.newException((ErrorResponse) resp);
+        }
+        
+        return resp;
     }
 
     
@@ -247,16 +283,15 @@ public final class CPath2Client
 	 * Builds a 'NEIGHBORHOOD' BioPAX <em>graph</em> query URL string.
 	 * 
 	 * @param sourceSet
-	 * @param direction
 	 * @return
 	 */
-	public String queryNeighborhood(Collection<String> sourceSet, Direction direction)
+	public String queryNeighborhood(Collection<String> sourceSet)
 	{
 		return endPointURL + Cmd.GRAPH + "?" + CmdArgs.kind + "=" +
 			GraphType.NEIGHBORHOOD.name().toLowerCase() + "&"
-			+ join(CmdArgs.source + "=", sourceSet, "&") + "&"
-			+ CmdArgs.direction + "=" + direction + "&"
-			+ CmdArgs.limit + "=" + graphQueryLimit;
+			+ join(CmdArgs.source + "=", sourceSet, "&")
+			+ "&" + CmdArgs.direction + "=" + direction 
+			+ "&" + CmdArgs.limit + "=" + graphQueryLimit;
 	}
 
 	
@@ -267,9 +302,9 @@ public final class CPath2Client
 	 * @param direction direction to extends network towards neighbors
 	 * @return BioPAX model representing the neighborhood.
 	 */
-	public Model getNeighborhood(Collection<String> sourceSet, Direction direction)
+	public Model getNeighborhood(Collection<String> sourceSet)
 	{
-		String url = queryNeighborhood(sourceSet, direction);
+		String url = queryNeighborhood(sourceSet);
 		return restTemplate.getForObject(url, Model.class);
 	}
 
@@ -277,11 +312,12 @@ public final class CPath2Client
 	/**
 	 * Builds a 'COMMON STREAM' BioPAX <em>graph</em> query URL string.
 	 * 
+	 * @see #setDirection(Direction)
+	 * 
 	 * @param sourceSet
-	 * @param direction
 	 * @return
 	 */
-	public String queryCommonStream(Collection<String> sourceSet, Direction direction)
+	public String queryCommonStream(Collection<String> sourceSet)
 	{
 		if (direction == Direction.BOTHSTREAM)
 			throw new IllegalArgumentException(
@@ -289,9 +325,9 @@ public final class CPath2Client
 
 		return endPointURL + Cmd.GRAPH + "?" + CmdArgs.kind + "=" +
 			GraphType.COMMONSTREAM.name().toLowerCase() + "&"
-			+ join(CmdArgs.source + "=", sourceSet, "&") + "&"
-			+ CmdArgs.direction + "=" + direction + "&"
-			+ CmdArgs.limit + "=" + graphQueryLimit;
+			+ join(CmdArgs.source + "=", sourceSet, "&") 
+			+ "&" + CmdArgs.direction + "=" + direction 
+			+ "&" + CmdArgs.limit + "=" + graphQueryLimit;
 	}
 	
 	
@@ -299,13 +335,18 @@ public final class CPath2Client
 	 * This query searches for the common upstream (common regulators) or
 	 * common downstream (common targets) objects of the given source set.
 	 *
+	 * @see #setDirection(Direction) (only upstream or downstream)
+	 *
 	 * @param sourceSet set of physical entities
-	 * @param direction upstream or downstream
 	 * @return a BioPAX model that contains the common stream
 	 */
-	public Model getCommonStream(Collection<String> sourceSet, Direction direction)
+	public Model getCommonStream(Collection<String> sourceSet)
 	{
-		String url = queryCommonStream(sourceSet, direction);
+		if (direction == Direction.BOTHSTREAM)
+			throw new IllegalArgumentException(
+				"Direction of common-stream query should be either upstream or downstream.");
+		
+		String url = queryCommonStream(sourceSet);
 		return restTemplate.getForObject(url, Model.class);
 	}
 
@@ -354,22 +395,11 @@ public final class CPath2Client
     }
     
     
-    /**
-     * Can generate stings like 
-     * "prefix=strings[1]&prefix=strings[2]..."
-     * (if the delimiter is '&')
-     * 
-     * @param prefix
-     * @param strings
-     * @param delimiter
-     * @return
-     */
     private String join(String prefix, Collection<String> strings, String delimiter) {
         List<String> prefixed = new ArrayList<String>();
 
-        for(String s: strings) {
-            prefixed.add(prefix + s);
-        }
+       	for(String s: strings)
+			prefixed.add(prefix + s);
 
         return StringUtils.join(prefixed, delimiter);
     }
@@ -400,7 +430,7 @@ public final class CPath2Client
      * Default is 0. Total number of result pages (P) can be calculated using the first
      * page SearchResponse attributes as follows: P = INT[ (numHits - 1)/numHitsPerPage + 1 ]
      *
-     * @see #search(java.util.Collection)
+     * @see #search(Collection)
      *
      * @return the page number
      */
@@ -424,8 +454,8 @@ public final class CPath2Client
     /**
      * Graph query search distance limit (default = 1).
      *
-     * @see #getNeighborhood(Collection, Direction)
-     * @see #getCommonStream(Collection, Direction)
+     * @see #getNeighborhood(Collection)
+     * @see #getCommonStream(Collection)
      * @see #getPathsBetween(Collection)
      *
      * @return distance limit.
@@ -578,4 +608,24 @@ public final class CPath2Client
 		this.path = path;
 	}
     
+		
+	/**
+	 * Graph query direction (depends on the graph query type)
+	 * 
+	 * @return the direction
+	 */
+	public Direction getDirection() {
+		return direction;
+	}
+
+
+	/**
+	 * @see #getDirection()
+	 * @param direction the direction to set (if null, then 'downstream' is used)
+	 */
+	public void setDirection(Direction direction) {
+		this.direction = (direction == null)
+			? Direction.DOWNSTREAM : direction;
+	}
+	
 }
