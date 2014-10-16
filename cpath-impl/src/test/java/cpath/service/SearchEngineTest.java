11d7e22012be02966be5864f2245d53f74bacc5a
rodche <rodche@gmail.com>
2014-10-16 14:19:19 -0400

    (Not finished) new code, for future versions, mainly to index/search a BioPAX model, not using Hibernate (Lucene only).

---
+++ cpath-impl/src/test/java/cpath/service/SearchEngineTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package cpath.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author rodche
+ *
+ */
+@Ignore //TODO implement, enable
+public class SearchEngineTest {
+
+	/**
+	 * Test method for {@link cpath.service.SearchEngine#search(java.lang.String, int, java.lang.Class, java.lang.String[], java.lang.String[])}.
+	 */
+	@Test
+	public final void testSearch() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cpath.service.SearchEngine#index()}.
+	 */
+	@Test
+	public final void testIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cpath.service.SearchEngine#indexExists(java.io.File)}.
+	 */
+	@Test
+	public final void testIndexExists() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
