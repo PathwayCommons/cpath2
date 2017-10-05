01a9830eb94f4402601553dca4ab0c2993d7af2b
IgorRodchenkov <rodche@gmail.com>
2017-10-05 16:42:08 -0400

    Finally, 'java -jar cpath2.war' can be run from anywhere (unlike packaging as jar...), as as 'mvn spring-boot:run' too.

--- cpath-ws/src/main/java/cpath/service/Application.java
+++ cpath-ws/src/main/java/cpath/service/Application.java
@@ -2,7 +2,9 @@ package cpath.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -16,11 +18,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @SpringBootApplication
-@EnableCaching(proxyTargetClass = true)
+//@EnableCaching(proxyTargetClass = true)
 @PropertySource(value = "file:cpath2.properties", ignoreResourceNotFound = true)
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"cpath.jpa"})
 public class Application {
+//        extends SpringBootServletInitializer {
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Application.class);
+//    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
