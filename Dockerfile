a826c445f231759124e5a35032af0b8fed4e9203
IgorRodchenkov <rodche@gmail.com>
2023-11-05 17:25:12 -0500

    Updated readme, dockerfile and docker run example, properties, logback conf.

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM eclipse-temurin:latest
 ARG APP_JAR
 # persistent volume containing the app data (biopax model ad index files;
 # can use target/work demo/test data)
-VOLUME /work
 COPY ${APP_JAR} cpath2.war
 ENV CPATH2_HOME /work
-ENV JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
+ENV JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
 ENTRYPOINT ["java", "-server", "-Djava.security.egd=file:/dev/./urandom", \
-"-Dfile.encoding=UTF-8", "-Xss32m", "-Xmx64g", "-Dspring.profiles.active=docker", "-jar", "cpath2.war", "--server"]
+"-Dfile.encoding=UTF-8", "-Xss32m", "-Xmx64g", \
+"-Dspring.profiles.active=docker", \
+"-Dpaxtools.model.safeset=list", \
+"-Dpaxtools.normalizer.use-latest-registry=true", \
+"-jar", "/cpath2.war", "--server"]
 EXPOSE 8080
--- work/cpath2.sh
+++ work/cpath2.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 export CPATH2_HOME="."
-JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
-CPATH2_OPTS="-Dfile.encoding=UTF-8 -Xss32m -Xmx60g"
-# CPATH2_DEBUG_OPTS="-Dlogback.configurationFile=logback.xml -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=12345"
+
+JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
+
+OPTS="-server -Dfile.encoding=UTF-8 -Xss32m -Xmx60g -Dpaxtools.normalizer.use-latest-registry=true"
+DEBUG_OPTS=""
+#DEBUG_OPTS="-Dlogback.configurationFile=logback.xml -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=12345"
+
+#BioPAX model is read-only (ok to use List instead Map for collections) when we run as Web app but not if building the instance/data
+export CPATH2_OPTS="$DEBUG_OPTS $OPTS -Dpaxtools.model.safeset=list"
 
 for arg in "$@"; do
   if [ "$arg" = "--build" ] || [ "$arg" = "-b" ] ; then
-    echo "enabled load time weaving"
-    CPATH2_OPTS="$CPATH2_OPTS -server -javaagent:../target/spring-instrument.jar"
+    echo "Enabled load time weaving (LTW)"
+    export CPATH2_OPTS="$DEBUG_OPTS $OPTS -Dpaxtools.model.safeset=map -javaagent:../target/spring-instrument.jar"
     break
   fi
 done
