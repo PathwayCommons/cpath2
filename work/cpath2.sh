dc1019e504a25e582cf92ebb88be5f6ff3100a38
IgorRodchenkov <rodche@gmail.com>
2024-02-10 20:36:52 -0500

    Improved the script: work/cpath2.sh

--- work/cpath2.sh
+++ work/cpath2.sh
@@ -3,20 +3,18 @@
 export CPATH2_HOME="."
 
 JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
-
-OPTS="-server -Dfile.encoding=UTF-8 -Xss32m -Xmx60g -Dpaxtools.normalizer.use-latest-registry=true"
 DEBUG_OPTS=""
 #DEBUG_OPTS="-Dlogback.configurationFile=logback.xml -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=12345"
-
-#BioPAX model is read-only (ok to use List instead Map for collections) when we run as Web app but not if building the instance/data
-export CPATH2_OPTS="$DEBUG_OPTS $OPTS -Dpaxtools.model.safeset=list"
+BASE_OPTS="-Dfile.encoding=UTF-8 -Xss32m -Xmx60g -Dpaxtools.normalizer.use-latest-registry=true"
+#use List instead Map collections for read-only BioPAX model if we start the Web app but not if building the instance/data
+EXTRA_OPTS="$BASE_OPTS -Dpaxtools.model.safeset=list -server"
 
 for arg in "$@"; do
   if [ "$arg" = "--build" ] || [ "$arg" = "-b" ] ; then
     echo "Enabled load time weaving (LTW)"
-    export CPATH2_OPTS="$DEBUG_OPTS $OPTS -Dpaxtools.model.safeset=map -javaagent:../target/spring-instrument.jar"
+    EXTRA_OPTS="$BASE_OPTS -Dpaxtools.model.safeset=map -javaagent:../target/spring-instrument.jar"
     break
   fi
 done
 
-$JAVA_HOME/bin/java $CPATH2_OPTS -jar ../target/cpath2.war "$@"
+$JAVA_HOME/bin/java $DEBUG_OPTS $EXTRA_OPTS -jar ../target/cpath2.war "$@"
