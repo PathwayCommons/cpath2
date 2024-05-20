c1e9a2b640c17f3480b6c2cb165248929b9e6937
IgorRodchenkov <rodche@gmail.com>
2024-05-20 10:05:52 -0400

    Updated export.sh

--- work/export.sh
+++ work/export.sh
@@ -7,15 +7,14 @@ $JAVA_HOME/bin/java -Xmx64g -Dpaxtools.CollectionProvider=org.biopax.paxtools.tr
 
 $JAVA_HOME/bin/java -Xmx64g -Dpaxtools.CollectionProvider=org.biopax.paxtools.trove.TProvider -Dpaxtools.normalizer.use-latest-registry=true -Dpaxtools.core.use-latest-genenames=true -jar paxtools.jar toSIF 'pc-biopax.owl.gz' 'pc-hgnc.txt' seqDb=hgnc -extended -andSif exclude=neighbor_of
 
-rename 's/txt\.sif/sif/' *.txt.sif
-gzip *.txt *.sif *.gmt
-
 $JAVA_HOME/bin/java -Xmx64g -Dpaxtools.CollectionProvider=org.biopax.paxtools.trove.TProvider -Dpaxtools.normalizer.use-latest-registry=true -Dpaxtools.core.use-latest-genenames=true -jar paxtools.jar summarize 'pc-biopax.owl.gz' 'pathways.txt' --pathways
 
 $JAVA_HOME/bin/java -Xmx64g -Dpaxtools.CollectionProvider=org.biopax.paxtools.trove.TProvider -Dpaxtools.normalizer.use-latest-registry=true -Dpaxtools.core.use-latest-genenames=true -jar paxtools.jar summarize 'pc-biopax.owl.gz' 'physical_entities.json' --uri-ids
 
 cat physical_entities.json | jq -cS 'map(select(.generic)) | reduce .[] as $o ({}; . + {($o.uri): {name: $o.name, label:$o.label, synonyms:$o."hgnc.symbol"}})' > generic-physical-entity-map.json
-gzip pathways.txt physical_entities.json
+
+rename 's/txt\.sif/sif/' *.txt.sif
+gzip *.txt *.sif *.gmt physical_entities.json
 
 cd ..
 echo "export.sh completed."
